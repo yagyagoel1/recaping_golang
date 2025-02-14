5b995836bf1d8e459c2534967fc7497c692f413c
yagyagoel1 <yagyagoel87@gmail.com>
2025-02-14 22:31:57 +0530

    learned about variadic_function and closures

---
+++ 13_variadic_functions/variadic.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func sum(nums ...int) int {
+	total := 0
+	for _, num := range nums {
+		total = total + num
+	}
+	return total
+
+}
+func main() {
+	fmt.Println(1, 2, 3, 5, 86, "hello")
+	nums := []any{3, 4, 5, 6}
+
+	// result := sum(nums...)
+	// fmt.Println(result)
+	fmt.Println(nums)
+	fmt.Println("")
+}
